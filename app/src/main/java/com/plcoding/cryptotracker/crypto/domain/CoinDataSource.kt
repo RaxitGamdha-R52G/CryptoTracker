d130203e551eb4e74fcd6e92d88a42571bcf0204
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-02-20 02:22:05 +0530

    add CoinListViewModel

--- app/src/main/java/com/plcoding/cryptotracker/crypto/domain/CoinDataSource.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/domain/CoinDataSource.kt
@@ -2,5 +2,5 @@ package com.plcoding.cryptotracker.crypto.domain
 import com.plcoding.cryptotracker.core.domain.util.NetworkError
 import com.plcoding.cryptotracker.core.domain.util.Result
 interface CoinDataSource {
-    suspend fun getCoints(): Result<List<Coin>, NetworkError>
+    suspend fun getCoins(): Result<List<Coin>, NetworkError>
 }
