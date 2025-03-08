f83d02161b0f3b60de1d6283a54508ba06834718
Raxit Gamdha <115684444+RaxitGamdha-R52G@users.noreply.github.com>
2025-03-09 02:41:09 +0530

    implement CoinPrice, CoinPriceDto, CoinResponseDto for fetching particular coinUi object data price history with timeStamp from api.

--- app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/dto/CoinPriceDto.kt
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/networking/dto/CoinPriceDto.kt
@@ -3,6 +3,7 @@ package com.plcoding.cryptotracker.crypto.data.networking.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class CoinsResponceDto(
-    val data: List<CoinDto>
+data class CoinPriceDto(
+    val priceUsd: Double,
+    val time: Long
 )
